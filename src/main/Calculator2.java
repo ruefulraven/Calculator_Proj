0a1c0c600c0870e539833223e1bf094b2d0f13c8
ruefulraven <axelbajan66@gmail.com>
2022-09-15 19:50:48 +0800

    updating functionalities

---
+++ src/main/Calculator2.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Calculator2 implements CalculatorMethods{
+
+	@Override
+	public int addition(int firstNumber, int secondNumber) {
+		int sum = firstNumber + secondNumber + 1;
+		return sum;
+	}
+
+	@Override
+	public int subtraction(int firstNumber, int secondNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int multiplication(int firstNumber, int secondNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int division(int firstNumber, int secondNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,6 +1,8 @@
 package main;
 
-public class MainClass {
+import java.util.Scanner;
+
+public class MainClass{
 
 	public static void main(String[] args) {
 		//Ask the user to input 2 numbers, ask the user to input either symbols + - * /
@@ -9,11 +11,30 @@ public class MainClass {
 		String symbol;
 		//Make this into a Dynamic Calculator that accepts user input
 		//Addition
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter first number:");
+		firstNumber = scanner.nextInt();
+		System.out.println("Enter second number:");
+		secondNumber = scanner.nextInt();
+		System.out.println("Enter calculator operation:");
+		symbol = scanner.next();
 		Calculator calculator = new Calculator();
-		System.out.println("This is Addition "  + calculator.addition(1, 1));
-		System.out.println("This is Subtraction "  + calculator.subtraction(1, 1));
-		System.out.println("This is Multiplication "  + calculator.multiplication(3, 3));
-		System.out.println("This is Division "  + calculator.division(20, 2));
+		Calculator2 calculator2 = new Calculator2();
+		switch(symbol) {
+			case "+":
+				System.out.println("This is Addition "  + calculator.addition(firstNumber, secondNumber));
+				System.out.println("This is Addition "  + calculator2.addition(firstNumber, secondNumber));
+				break;
+			case "-":
+				System.out.println("This is Subtraction "  + calculator.subtraction(firstNumber, secondNumber));
+				break;
+			case "*":
+				System.out.println("This is Multiplication "  + calculator.multiplication(firstNumber, secondNumber));
+				break;
+			case "/":
+				System.out.println("This is Division "  + calculator.division(firstNumber, secondNumber));
+				break;
+		}
+		scanner.close();
 	}
-
 }
